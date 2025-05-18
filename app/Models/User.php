176849cd4d8028603c284363f8ee267d35137670
GabrielManzanilla <gabriel29444@gmail.com>
2025-05-18 17:28:26 -0600

    chore(auth): configurar generación automática de UUID para usuarios
    Se añadió la configuración necesaria para que los usuarios utilicen UUID como identificador primario al ser creados.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,27 @@
 
 namespace App\Models;
 
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasUuids, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    public $keyType = 'string';
     protected $fillable = [
         'name',
         'email',
