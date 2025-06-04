d929f3e4f926e5dad41809f5f01fa8102c70ae73
GabrielManzanilla <gabriel29444@gmail.com>
2025-06-04 00:21:53 -0600

    feat(perfil): permitir actualización de la información del perfil del usuario
    Se modificó el método update en el PerfilController para manejar la actualización de los
    datos del perfil del usuario autenticado.

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PerfilUsuario;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -42,14 +43,14 @@ class PerfilController extends Controller
         } else {
             $user = auth()->user();
         }
-        
+
         $perfil = $user->perfil; // Accede a la relación como propiedad
-        
-        $acta_nacimiento=$perfil->files()->where('str_categoria_archivo', 'acta_nacimiento')->first();
-        $comprobante_domicilio=$perfil->files()->where('str_categoria_archivo', 'comprobante_domicilio')->first();
-        $ine=$perfil->files()->where('str_categoria_archivo', 'ine')->first();
-        
-        
+
+        $acta_nacimiento = $perfil->files()->where('str_categoria_archivo', 'acta_nacimiento')->first();
+        $comprobante_domicilio = $perfil->files()->where('str_categoria_archivo', 'comprobante_domicilio')->first();
+        $ine = $perfil->files()->where('str_categoria_archivo', 'ine')->first();
+
+
         return view('perfil.show', [
             'user' => $user,
             'perfil' => $perfil,
@@ -65,7 +66,7 @@ class PerfilController extends Controller
      * Show the form for editing the specified resource.
      */
     public function edit(string $id)
-    {   
+    {
         $user = User::findOrFail($id);
         $perfil = $user->perfil; // Accede a la relación como propiedad
 
@@ -81,7 +82,38 @@ class PerfilController extends Controller
     public function update(Request $request, string $id)
     {
         //
-        dd($request->all(), $id);
+        $perfil = PerfilUsuario::findOrFail($id);
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'apellido_paterno' => ['required', 'string', 'max:255'],
+            'apellido_materno' => ['required', 'string', 'max:255'],
+            'fecha_nacimiento' => ['required', 'date'],
+            'curp' => ['required', 'string', 'max:255'],
+            'municipio_nacimiento' => ['required', 'string', 'max:255'],
+            'estado_nacimiento' => ['required', 'string', 'max:255'],
+            'sexo' => ['required', 'string'],
+            'es_mayahablante' => ['required', 'boolean'],
+            'telefono' => ['required', 'string', 'max:20'],
+            'foto_perfil' => ['nullable', 'image', 'max:2048', 'mimes:jpg,jpeg,png'], // Max 2MB
+            'ine' => ['nullable', 'file', 'max:2048', 'mimes:pdf'], // Max 2MB, PDF only
+            'acta_nacimiento' => ['nullable', 'file', 'max:2048', 'mimes:pdf'], // Optional, Max 2MB, PDF only
+            'comprobante_domicilio' => ['nullable', 'file', 'max:2048', 'mimes:pdf'], // Optional, Max 2MB, PDF only
+        ]);
+        $perfil->update([
+            'str_nombre' => $request->name,
+            'str_apellido_paterno' => $request->apellido_paterno,
+            'str_apellido_materno' => $request->apellido_materno,
+            'dt_fecha_nacimiento' => $request->fecha_nacimiento,
+            'str_curp' => $request->curp,
+            'str_municipio_nacimiento' => $request->municipio_nacimiento,
+            'str_estado_nacimiento' => $request->estado_nacimiento,
+            'str_sexo' => $request->sexo,
+            'bool_es_mayahablante' => $request->es_mayahablante,
+            'str_telefono' => $request->telefono,
+        ]);
+
+        return redirect()->route('perfil.show');
+
     }
 
     /**
