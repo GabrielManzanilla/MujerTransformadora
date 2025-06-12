7ca4a89ece29768687d094ca884a9046e3294271
GabrielManzanilla <gabriel29444@gmail.com>
2025-06-11 23:07:50 -0600

    style(solicitud): ajustes mínimos en los estilos de la vista show
    Se realizaron cambios menores en los estilos de la vista de detalles de la solicitud
    para mejorar la presentación de la información.

--- resources/views/solicitud/show.blade.php
+++ resources/views/solicitud/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 	<div class=" h-full w-full overflow-auto p-2 md:p-5">
-		<div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 			<div class="bg-white overflow-hidden shadow-sm rounded-lg">
 				<div class="p-6 bg-white border-b border-gray-200">
 					<div class="flex flex-row gap-2 items-center mb-4">
@@ -10,15 +10,15 @@
 						</h2>
 					</div>
 					<div class="flex flex-col gap-4">
-						<div class="bg-gray-100 p-4 rounded-lg shadow flex-1">
-							<h3 class="font-bold text-lg mb-2">Información Fiscal</h3>
+						<div class="bg-gray-100 px-8 py-6 rounded-lg shadow flex-1">
+							<h3 class="font-bold text-lg mb-2 uppercase">Información Fiscal</h3>
 							<p><strong>Regimen: </strong>{{ $datoFiscal->str_regimen }}</p>
 							<p><strong>Actividad Economica: </strong>{{ $datoFiscal->str_actividad_economica }}</p>
 							<p><strong>Razon Social: </strong>{{ $datoFiscal->str_razon_social }}</p>
 							<p><strong>Cantidad de Empleados: </strong>{{ $datoFiscal->int_numero_empleados }}</p>
 						</div>
-						<div class="bg-gray-100 p-4 rounded-lg shadow flex-1">
-							<h3 class="font-bold text-lg mb-2">Credenciales</h3>
+						<div class="bg-gray-100 px-8 py-6 rounded-lg shadow flex-1 ">
+							<h3 class="font-bold text-lg mb-2 uppercase">Credenciales</h3>
 							<div class="grid grid-cols-3">
 								<p><strong>RFC: </strong>{{ $datoFiscal->str_clave_sat }}</p>
 								<p><strong>IMSS: </strong>{{ $datoFiscal->str_clave_imss }}</p>
@@ -44,6 +44,7 @@
 						</div>
 
 
+						<!-- Añadir seccion de los documentos -->
 					</div>
 				</div>
 			</div>
