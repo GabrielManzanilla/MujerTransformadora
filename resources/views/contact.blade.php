da73e39d31f6c2e865dee8d96517e4e07847448e
GabrielManzanilla <gabriel29444@gmail.com>
2025-08-17 23:49:31 -0600

    Agregando rutas de contacto y acerca de

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+	<div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="p-6">
+                    <h2 class="text-2xl font-bold mb-4">Contactos</h2>
+										<div>
+											<p>Esta es la página de Contactos.</p>
+										</div>
+                    <!-- Add your dashboard content here -->
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
